dcf262a179ff8c4e72d912e333c6031e05d7df4a
redbeardcr <redbeardcr@gmail.com>
2017-10-05 14:55:22 +0100

    add template folder add base.html add static folder + reference add css folder + css file add products table to admin

--- mystockproject/mystockproject/urls.py
+++ mystockproject/mystockproject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from stockcontrol import views
+
+
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
--- mystockproject/stockcontrol/models.py
+++ mystockproject/stockcontrol/models.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+    SKU = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
