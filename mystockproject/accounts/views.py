f5d31f053ed264b13e5f571b13bf3f344b5d258f
redbeardcr <redbeardcr@gmail.com>
2017-10-08 18:40:50 +0100

    signup.html now has css (doesn't look great)

--- mystockproject/accounts/views.py
+++ mystockproject/accounts/views.py
@@ -2,17 +2,18 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
